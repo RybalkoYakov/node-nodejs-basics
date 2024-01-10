9ae59eb59b1841c40d94f09964243c4f93b3f3c7
RybalkoYakov <RybalkoYakovv@gmail.com>
2024-01-11 00:12:02 +0300

    feat: copy function implement

--- src/fs/copy.js
+++ src/fs/copy.js
@@ -1,5 +1,48 @@
+import fs from 'fs/promises';
+import path from 'path';
+import { fileURLToPath } from 'url';
+
 const copy = async () => {
-    // Write your code here 
+    async function checkExistingFolders(writeFolderPath, readFolderPath) {
+        try {
+            await fs.access(readFolderPath, fs.constants.F_OK);
+        } catch (e) {
+            throw new Error(errorMessage);
+        }
+
+        try {
+            await fs.mkdir(writeFolderPath);
+        } catch (e) {
+            throw new Error(errorMessage);
+        }
+    }
+
+    async function copyFile(file) {
+        const filePath = path.join(readableFolderPath, file);
+        const fileContent = await fs.readFile(filePath, {encoding});
+
+        const writePath = path.join(writableFolderPath, file);
+
+        await fs.writeFile(writePath, fileContent, {encoding});
+    }
+
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
+    const readableFolderName = 'files';
+    const writableFolderName = 'files_copy';
+
+    const readableFolderPath = path.join(__dirname, readableFolderName);
+    const writableFolderPath = path.join(__dirname, writableFolderName);
+
+    const errorMessage = 'FS operation failed';
+    const encoding = 'utf-8';
+
+    await checkExistingFolders(writableFolderPath, readableFolderPath);
+
+    const files = await fs.readdir(readableFolderPath, {encoding});
+
+    files.forEach(copyFile);
 };
 
 await copy();
