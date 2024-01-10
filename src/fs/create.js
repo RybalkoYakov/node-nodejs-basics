5d4e2ddd2fdb4f613166867b8aa2a9632a643c64
RybalkoYakov <RybalkoYakovv@gmail.com>
2024-01-10 22:26:07 +0300

    fix: fix path in create function

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,12 +1,16 @@
 import fs from 'fs';
 import path from 'path';
+import { fileURLToPath } from 'url';
 
 const create = async () => {
+    const __filename = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__filename);
+
     const fileFolder = 'files';
     const fileName = 'fresh.txt';
     const fileContent = 'I am fresh and young';
     const errorMessage = 'FS operation failed';
-    const writePath = path.join(fileFolder, fileName);
+    const writePath = path.join(__dirname, fileFolder, fileName);
     const encoding = 'utf-8';
 
     const writeStream = fs.createWriteStream(writePath, {encoding, flags: 'wx'});
