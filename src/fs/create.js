805f1b42a3f58e88692209c0cd6f03f8b640de8a
RybalkoYakov <RybalkoYakovv@gmail.com>
2024-01-10 22:20:23 +0300

    feat: create function inmplement

--- src/fs/create.js
+++ src/fs/create.js
@@ -1,5 +1,21 @@
+import fs from 'fs';
+import path from 'path';
+
 const create = async () => {
-    // Write your code here 
+    const fileFolder = 'files';
+    const fileName = 'fresh.txt';
+    const fileContent = 'I am fresh and young';
+    const errorMessage = 'FS operation failed';
+    const writePath = path.join(fileFolder, fileName);
+    const encoding = 'utf-8';
+
+    const writeStream = fs.createWriteStream(writePath, {encoding, flags: 'wx'});
+
+    writeStream.write(fileContent, (error) => {
+        if (error) {
+            throw new Error(errorMessage);
+        }
+    })
 };
 
 await create();
