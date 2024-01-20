1bdd4a879819e418e80549e762c512b5774014a5
RybalkoYakov <RybalkoYakovv@gmail.com>
2024-01-20 14:58:55 +0300

    feat: function list implement

--- src/fs/list.js
+++ src/fs/list.js
@@ -1,5 +1,20 @@
+import fs from 'fs/promises';
+import {fileURLToPath} from 'url';
+import path from 'path';
+
 const list = async () => {
-    // Write your code here 
+    const __fileName = fileURLToPath(import.meta.url);
+    const __dirname = path.dirname(__fileName);
+
+    const scanningDirectory = 'files';
+    const errorMessage = 'FS operation failed';
+    const consoleColor = '\x1b[34m';
+
+    const pathToScanningDirectory = path.join(__dirname, scanningDirectory);
+
+    const files = await fs.readdir(pathToScanningDirectory);
+    if (files.length <= 0) throw new Error(errorMessage);
+    files.forEach(file => console.log(consoleColor, file));
 };
 
 await list();
