b20a424e0216f6a59868f5ee45b2bdee3b29bce9
RybalkoYakov <RybalkoYakovv@gmail.com>
2024-01-21 16:02:16 +0300

    fix: optimize env function and make output correct

--- src/cli/env.js
+++ src/cli/env.js
@@ -1,16 +1,10 @@
-
-
 const parseEnv = () => {
     const envVars = process.env;
     const prefix = 'RSS_';
 
-    const parsedObject = {};
-
     Object.keys(envVars).forEach(key => {
-        parsedObject[`${prefix}${key}`] = envVars[key];
+        console.log(`${prefix}${key}=${envVars[key]}`);
     })
-
-    console.log(parsedObject);
 };
 
 parseEnv();
